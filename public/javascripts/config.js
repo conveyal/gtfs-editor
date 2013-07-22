167f9e28f74b6cf43fe71af8e87cc3b20ec06534
Kevin Webb <kpwebb@gmail.com>
2013-07-22 03:08:42 -0400

    move conf.js to repo

---
+++ public/javascripts/config.js
@@ -0,0 +1,14 @@
+var GtfsEditor = GtfsEditor || {};
+
+(function(G, $) {
+
+  G.config = {
+  	mapboxKey: 'conveyal.map-jc4m5i21',
+    agencyId: 1,
+    mapCenter: [14.666217, 120.978355],
+    mapZoom: 15, 
+    showMajorStops: true,
+    showStandardStops: true
+  };
+
+})(GtfsEditor, jQuery);
