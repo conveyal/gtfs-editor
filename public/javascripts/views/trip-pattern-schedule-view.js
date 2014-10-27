c17beec55a52e96ade1f23c90722d47f45145386
Matthew Wigginton Conway <matt@indicatrix.org>
2014-10-27 11:41:34 -0400

    Allow editing cells, with many possible formats.

--- public/javascripts/views/trip-pattern-schedule-view.js
+++ public/javascripts/views/trip-pattern-schedule-view.js
@@ -1,173 +1,234 @@
 /**
-* A timetable view of a TripPattern and Calendar.
-*/
+ * A timetable view of a TripPattern and Calendar.
+ */
 
 var GtfsEditor = GtfsEditor || {};
 
-(function (G, $, ich) {
-  G.stopTimeRenderer = function (instance, td, row, col, prop, value, cellProperties) {
+(function(G, $, ich) {
+  var displayTime = function(st, arr) {
     // TODO: 12h time
     // TODO: single-time view
-    // value is stoptime
+
+    var time = arr ? st.arrivalTime : st.departureTime
 
     var text;
 
-    if (_.isUndefined(value)) {
+    if (_.isUndefined(time)) {
       text = '-';
-    }
-    else {
-      var arr = col % 2 == 1;
-      var time = arr ? value.arrivalTime : value.departureTime;
-
-
+    } else {
       var secs = time % 60;
       var mins = (time - secs) % (60 * 60) / 60;
       var hours = (time - secs - mins * 60) / (60 * 60);
 
+      // TODO: template
       text =
-      '<span class="hours">' + hours + '</span>' +
-      '<span class="minutes">' + (mins < 10 ? '0' + mins : mins) + '</span>' +
-      '<span class="seconds">' + (secs < 10 ? '0' + secs : secs) + '</span>';
+        '<span class="time ' + (arr ? 'time-arr' : 'time-dep') +
+        // dim departure times that are the same as their arrival times
+        // TODO: only in two-time mode
+        (!arr && st.departureTime == st.arrivalTime ? 'time-dep-dimmed' : '') + '">' +
+        '<span class="hours">' + hours + '</span>' +
+        '<span class="minutes">' + (mins < 10 ? '0' + mins : mins) + '</span>' +
+        '<span class="seconds">' + (secs < 10 ? '0' + secs : secs) + '</span>';
 
-      $(td).addClass('time');
-
-      $(td).addClass(arr ? 'time-arr' : 'time-dep');
-
-      // dim departure times that are the same as their arrival times
-      if (!arr && value.departureTime == value.arrivalTime)
-        $(td).addClass('time-dep-dimmed');
-      }
-
-      Handsontable.renderers.HtmlRenderer(instance, td, row, col, prop, text, cellProperties);
+      return text;
+    }
   };
 
+  /**
+   * Parse time in many formats
+   * return null if there is no time
+   * No time means "the vehicle does stop here, but it's up to the consumer to interpolate when"
+   */
+  var parseTime = function(time) {
+    // TODO: many time formats
+    // 332p
+    // 1532
+    // 153200
+    // 15:33
+    // 3:33 PM
+    // etc.
+    // TODO: validation
+    if (time == '')
+      return null;
+
+    // get things into a standard format
+    // everything lower case
+    // no whitespace
+    // get rid of the m on am, pm, and mm (post midnight)
+    // we replace semicolons with colons, to allow folks to enter 3;32;21 pm, to be easier on the fingers
+    // than having to type shift each time
+    time = time.toLowerCase().replace(/([apm])(m?)/, '$1').replace(';', ':').replace(/\W/g, '');
+
+    // separate the numbers from the letter
+    var match = /([0-9:]{3,})([apm]?)/.exec(time);
+
+    // figure out how much to add on for AM/PM/past-midnight
+    var additionalSeconds = 0;
+    if (match[2] == 'p')
+      additionalSeconds = 12 * 60 * 60;
+    else if (match[2] == 'm')
+      additionalSeconds = 24 * 60 * 60;
+
+    // am and 24-hour cases are implied
+
+    // parse out hours, minutes and seconds
+    var rawTime = match[1];
+    var hours, minutes, seconds = 0;
+
+    // if we have colons, use them as separators
+    if (rawTime.indexOf(':') != -1) {
+      var components = rawTime.split(':');
+      hours = Number(components[0]);
+      minutes = Number(components[1]);
+      seconds = components.length >= 3 ? Number(components[2]) : 0;
+    } else {
+
+      // no colons, infer things positionally
+      if (rawTime.length >= 5) {
+        // we have seconds
+        seconds = Number(rawTime.slice(-2));
+        rawTime = rawTime.slice(0, -2);
+      }
+
+      var minutes = Number(rawTime.slice(-2));
+      var hours = Number(rawTime.slice(0, -2));
+    }
+
+    // patch up 12am, 12pm, since they break the pattern
+    if (hours == 12 && match[2] == 'p')
+    // 12pm is noon
+      additionalSeconds = 0;
+
+    if (hours == 12 && match[2] == 'a')
+    // 12am is 00:00:00
+      additionalSeconds = -12 * 60 * 60;
+
+    return hours * 60 * 60 + minutes * 60 + seconds + additionalSeconds;
+  };
+
+  /**
+   * all of the valid time formats:
+   * 1342 332p 332 PM 1:01:03 PM 10:10:10 etc.
+   * 332m means 3 hours, thirty-two minutes past midnight and is used for overnight runs
+   * (e.g. starting at 11:55 PM and ending at 1:31 M, or 1:31 the next day).
+   *
+   * Note that 2090102 is not a valid time, while 209:01:02 is. Though 209 hours past midnight on the day
+   * of the start of the trip is in fact a valid time, it's almost always a typo when entered without colons, so
+   * we disallow. If you're running the trans-Siberian railway (or another long-distance service), you'll just
+   * have to enter the colons.
+   *
+   * Semicolons and colons are treated as equivalent, for ease of typing.
+   */
+  var validTimeFormat =
+    /^\W*[0-9]{3,6}\W*[apmAPM]?[mM]?\W*$|^\W*[0-9]{1,}[:;][0-9]{2}([:;][0-9]{2})?\W*[apmAPM]?[mM]?\W*$/;
+
   G.TripPatternScheduleView = Backbone.View.extend({
-    initialize: function (attr) {
+    initialize: function(attr) {
       // consistency check
       var tripPatternId = null;
       var serviceCalendarId = null;
       var first = true;
-      this.collection.each(function (trip) {
+      this.collection.each(function(trip) {
         if (first) {
           tripPatternId = trip.get('pattern').id;
           serviceCalendarId = trip.get('serviceCalendar').id;
           first = false;
-        }
-        else {
+        } else {
           if (trip.get('pattern').id != tripPatternId ||
-                trip.get('serviceCalendar').id != serviceCalendarId) {
+            trip.get('serviceCalendar').id != serviceCalendarId) {
             throw new Error("Trip pattern or service calendars differ between trips");
           }
         }
       });
     },
 
-    // parse out a time entered by a human and return seconds since (noon - 12h)
-    parseTime: function (time) {
-      // TODO: many time formats
-      // 332p
-      // 1532
-      // 153200
-      // 15:33
-      // 3:33 PM
-      // etc.
-      var spTime = time.split(':');
-      return spTime[0] * 3600 + spTime[1] * 60 + spTime[2] * 1;
+    // combination of getAttr and setAttr for handsontable
+    attr: function(name) {
+      var instance = this;
+      var ret = {
+        data: function(trip, val) {
+          if (name.indexOf('stop:') === 0) {
+            // we need to return a stop time, so first parse out the column header
+            var sp = name.split(':');
+            var stopId = sp[1];
+            var stopSeq = sp[2];
+            var arr = sp[3] == 'arr';
+
+            // find the stopTime with this stop id and stop sequence
+            // note that this makes an assumption about stop_sequence: when a stop is ommitted,
+            // its sequence number is omitted as well
+            var st = _.find(trip.get('stopTimes'), function(st) {
+              return st.stop.id == stopId && st.stopSequence == stopSeq;
+            });
+
+            if (_.isUndefined(val)) {
+              return displayTime(st, arr);
+            } else {
+              if (arr) {
+                st.arrivalTime = parseTime(val);
+              } else {
+                st.departureTime = parseTime(val);
+              }
+            }
+          } else if (name == 'trip_id') {
+            return trip.get('gtfsTripId');
+          } else if (name == 'block_id') {
+            return trip.get('blockId');
+          } else if (name == 'trip_name') {
+            return trip.get('tripName');
+          }
+        },
+      };
+
+      if (name.indexOf('stop:') === 0) {
+        ret.validator = validTimeFormat;
+        ret.renderer = Handsontable.renderers.HtmlRenderer
+      }
+
+      return ret;
     },
 
-    // combination of getAttr and setAttr for handsontable
-    attr: function (name) {
+    render: function() {
+      // render the template
+      this.$el.html(ich['timetable-tpl']());
+
+      // figure out what columns we're rendering
+      var columns = [this.attr('trip_id'), this.attr('block_id'), this.attr('name')];
+      // TODO: i18n
+      var headers = ['Trip ID', 'Block ID', 'Trip name'];
+
       var instance = this;
-      return {data: function (trip, val) {
-        if (name.indexOf('stop:') === 0) {
-          // we need to return a stop time, so first parse out the column header
-          var sp = name.split(':');
-          var stopId = sp[1];
-          var stopSeq = sp[2];
-          var arr = sp[3] == 'arr';
 
-          // find the stopTime with this stop id and stop sequence
-          // note that this makes an assumption about stop_sequence: when a stop is ommitted,
-          // its sequence number is omitted as well
-          var st = _.find(trip.get('stopTimes'), function (st) {
-            return st.stop.id == stopId && st.stopSequence == stopSeq;
-          });
+      // note: the merged header cells aren't actually merged, but depend on a really awful hack (in timetable.css) where the width of
+      // two columns is hardcoded as the header width. As a side effect, these numbers must be exactly twice the width of the time
+      // display cells
+      var colWidths = [150, 150, 150];
 
-          if (_.isUndefined(val)) {
-            return st;
-          }
-          else {
-            if (arr) {
-              st.arrivalTime = instance.parseTime(val) || st.arrivalTime;
-            }
-            else {
-              st.departureTime = instance.parseTime(val) || st.departureTime;
-            }
-            // TODO: save here
-          }
-        }
+      _.each(this.collection.at(0).get('pattern').patternStops, function(patternStop, idx) {
+        // we put stopSequence here for loop routes
+        columns.push(instance.attr('stop:' + patternStop.stop.id + ':' + patternStop.stopSequence + ':arr'));
+        columns.push(instance.attr('stop:' + patternStop.stop.id + ':' + patternStop.stopSequence + ':dep'));
+        headers.push(patternStop.stop.stopName);
+        // dummy header, will be overwritten when cells are merged
+        headers.push('');
 
-        else if (name == 'trip_id') {
-          return trip.get('gtfsTripId');
-        }
+        colWidths.push(75);
+        colWidths.push(75);
+      });
 
-        else if (name == 'block_id') {
-          return trip.get('blockId');
-        }
-
-        else if (name == 'trip_name') {
-          return trip.get('tripName');
-        }
-      },
-      renderer: name.indexOf('stop:') === 0 ? G.stopTimeRenderer : Handsontable.renderers.TextRenderer
-    };
-  },
-
-  render: function () {
-    // render the template
-    this.$el.html(ich['timetable-tpl']());
-
-    // figure out what columns we're rendering
-    var columns = [this.attr('trip_id'), this.attr('block_id'), this.attr('name')];
-    // TODO: i18n
-    var headers = ['Trip ID', 'Block ID', 'Trip name'];
-
-    var instance = this;
-
-    // we generate a mergecells array to tell the table to merge the stop headers for arr and dep
-    var mergeCells = [];
-    // note: the merged header cells aren't actually merged, but depend on a really awful hack (in timetable.css) where the width of
-    // two columns is hardcoded as the header width. As a side effect, these numbers must be exactly twice the width of the time
-    // display cells
-    var colWidths = [150, 150, 150];
-
-    _.each(this.collection.at(0).get('pattern').patternStops, function (patternStop, idx) {
-      // we put stopSequence here for loop routes
-      columns.push(instance.attr('stop:' + patternStop.stop.id + ':' + patternStop.stopSequence + ':arr'));
-      columns.push(instance.attr('stop:' + patternStop.stop.id + ':' + patternStop.stopSequence + ':dep'));
-      headers.push(patternStop.stop.stopName);
-      // dummy header, will be overwritten when cells are merged
-      headers.push('');
-
-      // merge the two created header cells. idx + 4 is because there are three columns before the times,
-      // and columns are 1-based
-      mergeCells.push({row: -1, col: idx + 3, rowspan: 1, colspan: 2})
-      colWidths.push(75);
-      colWidths.push(75);
-    });
-
-    var $container = this.$('#timetable');
-    $container.handsontable({
-      data: this.collection.toArray(),
-      dataSchema: function () { return new G.Trip(); },
-      // we'll be defining interaction on our own
-      contextMenu: false,
-      columns: columns,
-      colHeaders: headers,
-      colWidths: colWidths
-      //mergeCells: mergeCells
-    });
-    return this;
-  }
-});
+      var $container = this.$('#timetable');
+      $container.handsontable({
+        data: this.collection.toArray(),
+        dataSchema: function() {
+          return new G.Trip();
+        },
+        // we'll be defining interaction on our own
+        contextMenu: false,
+        columns: columns,
+        colHeaders: headers,
+        colWidths: colWidths
+      });
+      return this;
+    }
+  });
 })(GtfsEditor, jQuery, ich);
