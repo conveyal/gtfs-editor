59cf6f0b48702cd471ee6ad5905400e10e47b049
David Emory <demory@conveyal.com>
2016-02-12 10:30:25 -0500

    use window.location.replace()

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -7,10 +7,13 @@ var GtfsEditor = GtfsEditor || {};
   	mapboxSatelliteKey: 'conveyal.map-a3mk3jug',
     agencyId: 1,
     mapCenter: [38.923092, -77.038879],
-    mapZoom: 15, 
+    mapZoom: 15,
     showMajorStops: true,
     showStandardStops: true,
-    baseUrl: '/'
+    baseUrl: '/',
+    auth0ClientId: 'dR7GdOhtI3HFNxfm4HySDL4Ke8uyGfTe',
+    auth0Domain: 'conveyal.eu.auth0.com',
+    projectId: '06ddd58a-3275-48d6-9b50-1d46c7b24c8e'
   };
 
 })(GtfsEditor, jQuery);
