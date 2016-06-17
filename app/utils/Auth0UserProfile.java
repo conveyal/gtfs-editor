a8025dc1a1067d9d944acc643da3d8852431d477
Landon Reed <landontreed@gmail.com>
2016-06-17 12:07:03 -0400

    import play

--- app/utils/Auth0UserProfile.java
+++ app/utils/Auth0UserProfile.java
@@ -1,5 +1,6 @@
 package utils;
 
+import play.Play;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
