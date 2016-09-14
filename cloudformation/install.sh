291357d4ad599cc4b81fc469dca22251bb8d9eb6
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-09-14 11:53:52 -0400

    deploy pointer to dev: conveyal->bruno

--- cloudformation/install.sh
+++ cloudformation/install.sh
@@ -3,7 +3,7 @@ sudo apt-get --yes update
 sudo apt-get --yes install git unzip openjdk-7-jre
 wget http://downloads.typesafe.com/releases/play-1.2.5.zip
 unzip play-1.2.5.zip
-git clone https://github.com/conveyal/gtfs-editor.git
+git clone https://github.com/brunosan/gtfs-editor.git
 cd gtfs-editor
 cp conf/application.conf.template conf/application.conf
 ../play-1.2.5/play dependencies
